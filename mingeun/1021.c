54172c505ce5dfd51198a08b35dce4fd05171c05
Mingeun Kim <getsolaris@naver.com>
2017-05-30 21:30:23 +0900

    Add files via upload

---
+++ mingeun/1021.c
@@ -0,0 +1,16 @@
+//
+//  1021.c
+//  c language
+//
+//  Created by Mingeun Kim on 2017. 5. 30..
+//  Copyright © 2017년 Mingeun Kim. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    char a[51];
+    scanf("%s", a);
+    printf("%s", a);
+    return 0;
+}
