5893286790ae3aa99e0a9a5c439a43ed572a244f
Mingeun Kim <getsolaris@naver.com>
2017-05-30 20:50:43 +0900

    Add files via upload

---
+++ mingeun/1012.c
@@ -0,0 +1,17 @@
+//
+//  1012.c
+//  c language
+//
+//  Created by Mingeun Kim on 2017. 5. 30..
+//  Copyright © 2017년 Mingeun Kim. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    float a;
+    scanf("%f",&a);
+    printf("%f", a);
+    return 0;
+}
