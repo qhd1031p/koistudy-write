26ef1bbb1882ceaa32f837e1ca2a5f109b4718c4
Dabin Kim <dabin0450@naver.com>
2017-06-06 21:19:17 +0900

    Add files via upload

---
+++ dabin/[기초-논리연산] 둘 다 거짓일 경우만 참 출력하기.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int a,b=0;
+	scanf("%d %d",&a,&b);
+	if(a==0&&b==0){
+	printf("1");}
+	else{printf("0");}
+	return 0;
+	}
---
+++ dabin/[기초-산술연산] 정수 두 개 입력받아 자동 계산하기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int a,b=0;
+	scanf("%d %d",&a,&b);
+	printf("%d\n",a+b);
+	printf("%d\n",a-b);
+	printf("%d\n",a*b);
+	printf("%d\n",a/b);
+	printf("%d\n",a%b);
+	printf("%.2lf\n",(float)a/b);
+	return 0;
+	}
+	
